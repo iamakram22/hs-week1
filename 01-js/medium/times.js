4f8495b3cac676b2af32d7595dae2c298d2e8d73
Mohd Akram <iamakram22@gmail.com>
2024-08-04 21:21:10 +0530

    solved 01-medium times

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,14 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let startTime = new Date().getTime();
+    let sum = 0;
+    for(let i = 0; i < n; i++) {
+        sum += i;
+    }
+    let endTime = new Date().getTime();
+    let diff = (endTime - startTime) / 1000;
+    return diff;
 }
+
+console.log(calculateTime(100000));
