b845da149316bc5bd8b24f95c4a4eb2b179d2a8f
Mohd Akram <iamakram22@gmail.com>
2024-08-04 21:08:46 +0530

    solved 01-medium palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -6,8 +6,28 @@
   - `npm run test-palindrome`
 */
 
+function strReverse(string) {
+  let newString = '';
+  for(let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
+}
+
+function strTransform(string) {
+  const special = [' ', ',', '!', '?', '.'];
+  let newString = '';
+  for(let i = 0; i < string.length; i++) {
+    newString += special.indexOf(string[i]) !== -1 ? '' : string[i];
+  }
+  return newString;
+}
+
 function isPalindrome(str) {
-  return true;
+  str = str.toLowerCase();
+  str = strTransform(str);
+  let reverse = strReverse(str);
+  return str === reverse;
 }
 
 module.exports = isPalindrome;
